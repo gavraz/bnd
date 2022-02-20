cf1b62273258b29341270da402b22a68a8bb1335
Resized <shai.yarden@gmail.com>
2022-02-20 22:32:33 +0200

    added simple Pixel imports and usage

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,13 @@ package main
 import (
 	"bnd/menu"
 	"fmt"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
 	term "github.com/nsf/termbox-go"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 )
 
 func drawMenu(h *menu.Handler) {
@@ -33,6 +39,31 @@ func handleInput(menuHandler *menu.Handler) {
 	}
 }
 
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Balls n' Dongs",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	basicTxt := text.New(pixel.V(100, 500), basicAtlas)
+
+	win.Clear(colornames.Skyblue)
+	fmt.Fprintln(basicTxt, "Hello, text!")
+	fmt.Fprintln(basicTxt, "I support multiple lines!")
+	fmt.Fprintf(basicTxt, "And I'm an %s, yay!", "io.Writer")
+
+	for !win.Closed() {
+		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 4))
+		win.Update()
+	}
+}
+
 func main() {
 	fmt.Println("Client: Hello B&D")
 
@@ -40,6 +71,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	pixelgl.Run(run)
 
 	defer term.Close()
 
--- go.mod
+++ go.mod
@@ -4,4 +4,17 @@ go 1.17
 
 require github.com/nsf/termbox-go v1.1.1
 
-require github.com/mattn/go-runewidth v0.0.9 // indirect
+require (
+	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
+	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
+	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72 // indirect
+	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+)
+
+require (
+	github.com/faiface/pixel v0.10.0
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+)
