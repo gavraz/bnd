3c2b8d5dd6721b2e26f95bcd1e806b40b05cefa9
Resized <shai.yarden@gmail.com>
2022-02-21 21:12:36 +0200

    Added pixelDisplay interface

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bnd/display"
 	"bnd/menu"
+	"bnd/pixelDisplay"
 	"fmt"
+	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	"github.com/faiface/pixel/text"
 	term "github.com/nsf/termbox-go"
-	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
 )
 
 func handleInput(menuHandler *menu.Handler) {
@@ -26,28 +24,43 @@ func handleInput(menuHandler *menu.Handler) {
 	}
 }
 
+func pixelHandleInput(menuHandler *menu.Handler, win *pixelgl.Window) {
+	if win.JustPressed(pixelgl.KeyS) || win.JustPressed(pixelgl.KeyDown) {
+		menuHandler.NextChoice()
+	}
+	if win.JustPressed(pixelgl.KeyW) || win.JustPressed(pixelgl.KeyUp) {
+		menuHandler.PrevChoice()
+	}
+	if win.JustPressed(pixelgl.KeyEnter) {
+		menuHandler.Choose()
+	}
+	if win.JustPressed(pixelgl.KeyEscape) || win.JustPressed(pixelgl.KeyBackspace) {
+		menuHandler.GoBack()
+	}
+}
+
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Balls n' Dongs",
 		Bounds: pixel.R(0, 0, 1024, 768),
-		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	basicTxt := text.New(pixel.V(100, 500), basicAtlas)
+	menuHandler := buildMenuHandler()
+	displayHandler := pixelDisplay.New()
 
-	win.Clear(colornames.Skyblue)
-	fmt.Fprintln(basicTxt, "Hello, text!")
-	fmt.Fprintln(basicTxt, "I support multiple lines!")
-	fmt.Fprintf(basicTxt, "And I'm an %s, yay!", "io.Writer")
+	fps := time.Tick(time.Second / 60)
 
 	for !win.Closed() {
-		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 4))
+		displayHandler.DrawMenu(menuHandler, win)
+		pixelHandleInput(menuHandler, win)
+
 		win.Update()
+
+		<-fps
 	}
 }
 
@@ -63,12 +76,14 @@ func main() {
 
 	defer term.Close()
 
-	menuHandler := buildMenuHandler()
-	displayHandler := display.New(5)
+	/*
+		menuHandler := buildMenuHandler()
+		displayHandler := display.New(5)
 
-	for {
-		displayHandler.DrawMenu(menuHandler)
-		handleInput(menuHandler)
-	}
+		for {
+			displayHandler.DrawMenu(menuHandler)
+			handleInput(menuHandler)
+		}
+	*/
 
 }
